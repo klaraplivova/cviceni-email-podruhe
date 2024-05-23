13edca7608b55295eb99835e6adec5e39c889c8f
klaraplivova <126472015+klaraplivova@users.noreply.github.com>
2024-05-23 19:04:59 +0200

    task finished

--- index.js
+++ index.js
@@ -2,13 +2,28 @@ const goodbye = (name) => {
   return 'S pozdravem ' + name;
 };
 
+const formalGoodbye = (name) => {
+  return `
+    S uctivou poklonou ${name}
+  `;
+}
+
+const rudeGoodbye = (name) => {
+  return "Se měj.. " + name
+}
+
 const fillSubject = (subject) => {
   document.querySelector('.email__subject').textContent = subject;
 };
 
-const fillBody = (body, name) => {
+const fillBody = (body, name, goodbyeFunction) => {
   const bodyElm = document.querySelector('.email__body');
   bodyElm.innerHTML = body;
   const closingElm = document.querySelector('.email__closing');
-  closingElm.textContent = goodbye(name);
+  closingElm.textContent = goodbyeFunction(name);
 };
+
+//ukol email2 lekce08
+
+fillSubject('Pozvánka na oslavu narozenin');
+fillBody('Zítra oslava. 18:00 ve Starý hospodě.', 'Patrik Veselý', rudeGoodbye);
